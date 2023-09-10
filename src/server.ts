886afa64e5f26827edceea7f4d5ef0638a2fcc0f
Andrew Balanescu <abalanescu@alphabroder.com>
2023-09-10 17:20:47 -0500

    🚀

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
 import express, { Request, Response, NextFunction } from 'express';
-import { getProfile, postProfile } from './controllers/gitController';
-import { logger  } from './config/logger';
+import { loggerMiddleware } from './controllers/gitController';
+// ************ 1. Import the router ************
 import indexRouter from './routes/index';
 import userRoutes from './routes/userRoutes';
 import productRoutes from './routes/productRoutes';
-import { loggerMiddleware } from './controllers/gitController';
+
 
 const app = express();
 const port = 3000;
-
+// ************ middlewares ************
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(loggerMiddleware);
+
+// ************ use router ************
 app.use('/', indexRouter);
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
